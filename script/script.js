090e2bd479ec3f97b5624b126d7d5094494971dc
Ali Shouman <alishouman010@gmail.com>
2022-09-16 18:48:22 +0300

    reset

--- script/script.js
+++ script/script.js
@@ -1,5 +1,12 @@
 const images = document.querySelectorAll(".unknown");
 const scoreTag = document.querySelector("#score");
+const reset = document.querySelector("#reset");
+
+// index of clicked img
+let fistImg = -1;
+let secondImg = -1;
+let score = 0;
+// path of needed img
 let photo = [
   "../assests/php.png",
   "../assests/java.png",
@@ -8,65 +15,55 @@ let photo = [
   "../assests/java.png",
   "../assests/js.png",
 ];
-let fistImg = -1;
-let secondImg = -1;
-let score = 0;
 
+//shuffle photo place
 function shuffle(array) {
   let currentIndex = array.length,
     randomIndex;
-
-  // While there remain elements to shuffle.
   while (currentIndex != 0) {
-    // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
-
-    // And swap it with the current element.
     [array[currentIndex], array[randomIndex]] = [
       array[randomIndex],
       array[currentIndex],
     ];
   }
-
   return array;
 }
-
 photo = shuffle(photo);
 
-const imgClick = (image, i) => {
-  image.classList.toggle("rotate");
-  setTimeout(() => playing(image, i), 800);
-};
-
+// close card if the 2 img no the same
 const resetImg = () => {
   images[fistImg].classList.toggle("rotate");
   images[secondImg].classList.toggle("rotate");
   images[fistImg].src = "../assests/unknown.png";
   images[secondImg].src = "../assests/unknown.png";
-
   fistImg = -1;
   secondImg = -1;
 };
 
 const playing = (image, i) => {
+  //the index of  clicked card/IMG will be the index of img behind/below the card in the "photo" array that will
+  // shuffled always when reset and each img will be exist 2 times in "photo" array
   image.src = photo[i];
   image.classList.toggle("rotate");
   if (fistImg == -1) {
+    //if pick one img
     fistImg = i;
   } else {
+    // if pick 2 img
     secondImg = i;
     if (photo[secondImg] == photo[fistImg]) {
       score++;
-      console.log(fistImg, images[fistImg]);
-      images[fistImg].removeEventListener("click", images[fistImg].fn);
-      images[secondImg].removeEventListener("click", images[secondImg].fn);
+      images[fistImg].removeEventListener("click", images[fistImg].ref);
+      images[secondImg].removeEventListener("click", images[secondImg].ref);
       fistImg = -1;
       secondImg = -1;
     } else {
       score--;
-      setTimeout(resetImg, 1000);
+      setTimeout(resetImg, 700);
     }
+    // score never be negative
     if (score < 0) {
       score = 0;
     }
@@ -74,6 +71,19 @@ const playing = (image, i) => {
   }
 };
 
+// addEventListener for all card/img
 images.forEach((image, i) => {
-  image.addEventListener("click", (image.fn = () => imgClick(image, i)));
+  image.addEventListener(
+    "click",
+    (image.ref = () => {
+      image.classList.toggle("rotate");
+      //to keep time for animation
+      setTimeout(() => playing(image, i), 500);
+    })
+  );
+});
+
+//reset will reload page readd all events and reset score
+reset.addEventListener("click", () => {
+  location.reload();
 });
