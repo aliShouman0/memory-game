cfd8fa1b4229f03796735db606a7aad0c28390ca
Ali Shouman <alishouman010@gmail.com>
2022-09-16 16:59:06 +0300

    score

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 const images = document.querySelectorAll(".unknown");
-const background = document.querySelector(".bg");
+const scoreTag = document.querySelector("#score");
 let photo = [
   "../assests/php.png",
   "../assests/java.png",
@@ -8,6 +8,9 @@ let photo = [
   "../assests/java.png",
   "../assests/js.png",
 ];
+let fistImg = -1;
+let secondImg = -1;
+let score = 0;
 
 function shuffle(array) {
   let currentIndex = array.length,
@@ -28,16 +31,49 @@ function shuffle(array) {
 
   return array;
 }
+
 photo = shuffle(photo);
 
-console.log(photo);
+const imgClick = (image, i) => {
+  image.classList.toggle("rotate");
+  setTimeout(() => playing(image, i), 800);
+};
+
+const resetImg = () => {
+  images[fistImg].classList.toggle("rotate");
+  images[secondImg].classList.toggle("rotate");
+  images[fistImg].src = "../assests/unknown.png";
+  images[secondImg].src = "../assests/unknown.png";
+
+  fistImg = -1;
+  secondImg = -1;
+};
+
+const playing = (image, i) => {
+  image.src = photo[i];
+  image.classList.toggle("rotate");
+  if (fistImg == -1) {
+    fistImg = i;
+  } else {
+    secondImg = i;
+    if (photo[secondImg] == photo[fistImg]) {
+      score++;
+      console.log(fistImg, images[fistImg]);
+      images[fistImg].removeEventListener("click", images[fistImg].fn);
+      images[secondImg].removeEventListener("click", images[secondImg].fn);
+      fistImg = -1;
+      secondImg = -1;
+    } else {
+      score--;
+      setTimeout(resetImg, 1000);
+    }
+    if (score < 0) {
+      score = 0;
+    }
+    scoreTag.textContent = score;
+  }
+};
+
 images.forEach((image, i) => {
-  image.addEventListener("click", () => {
-    image.classList.toggle("rotate");
-    setTimeout(() => {
-      image.src = photo[i];
-      console.log(photo[i-1],i);
-      image.classList.toggle("rotate");
-    }, 800);
-  });
+  image.addEventListener("click", (image.fn = () => imgClick(image, i)));
 });
